3f28b979749c4a8a4e87ddfa9b21aff03ea322c5
Corbin Crutchley <git@crutchcorn.dev>
2023-07-19 22:55:41 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,65 @@
+const source = `
+infer Test = 0;
+`;
+
+const defaultKeyword = {
+  match: /\w+/,
+  type: "Identifier"
+} as const;
+
+const breakKeywords = [{
+  match: /\W/,
+  type: "Whitespace"
+}] as const;
+
+const keywords = [{
+  match: /infer/,
+  type: "InferKeyword"
+}, {
+  match: /=/,
+  type: "EqualSign"
+}] as const;
+
+interface Token {
+  type: typeof keywords[number]["type"] | typeof defaultKeyword["type"];
+  val: string;
+}
+
+function tokenize(code: string) {
+  let currentToken = "";
+  let tokens: Token[] = [];
+  for (let i = 0; i < code.length; i++) {
+    const char = code[i];
+    if (!breakKeywords.some(keyword => keyword.match.test(char))) {
+      currentToken += char;
+      continue;
+    }
+    for (const keyword of keywords) {
+      if (!keyword.match.test(currentToken)) {
+        continue;
+      }
+      tokens.push({
+        type: keyword.type,
+        val: currentToken
+      });
+      currentToken = "";
+      break;
+    }
+    if (!currentToken) {
+      continue;
+    }
+    if (!defaultKeyword.match.test(currentToken)) {
+      throw new Error(`Unexpected token: ${currentToken}`);
+    }
+    tokens.push({
+      type: defaultKeyword.type,
+      val: currentToken
+    });
+    currentToken = "";
+  }
+
+  return tokens;
+}
+
+const tokens = tokenize(source);
+console.log(tokens)
